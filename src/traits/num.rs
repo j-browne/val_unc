61f82c64f7665d4f8e88a6a233635ff5f7542269
Justin Browne <jebdude89@gmail.com>
2020-02-05 09:56:00 -0500

    Fix clippy lint

--- src/traits/num.rs
+++ src/traits/num.rs
@@ -27,6 +27,7 @@ macro_rules! unc_zero_impl {
 unc_zero_impl!(usize, u8, u16, u32, u64, u128, isize, i8, i16, i32, i64, i128, f32, f64);
 
 impl UncZero for () {
+    #[allow(clippy::unused_unit)]
     fn zero() -> Self {
         ()
     }
